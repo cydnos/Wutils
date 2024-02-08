3f83f681b11a568687a7c5fbaa5ded734f37c842
cydnos <108056453+cydnos@users.noreply.github.com>
2024-02-09 00:01:18 +0100

    Create Deploy_ADDS.ps1

---
+++ Deploy_ADDS.ps1
@@ -0,0 +1,19 @@
+Script AD-DS
+
+#
+# Script Windows PowerShell pour le déploiement d’AD DS
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "M2l.net" `
+-DomainNetbiosName "M2L" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
