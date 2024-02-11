49077ea5b0a2b8156d90515c4842a9c0d4de43eb
cydnos <108056453+cydnos@users.noreply.github.com>
2024-02-11 12:53:57 +0100

    Create install_winget.ps1

---
+++ install_winget.ps1
@@ -0,0 +1,18 @@
+# installe le gestionnaire de paquets Windows (winget)
+
+# Vérifie si winget est déjà installé
+if (Get-Command winget -ErrorAction SilentlyContinue) {
+    Write-Output "Winget est déjà installé sur ce poste."
+} else {
+    # Si winget n'est pas installé, l'installe
+    # Télécharge l'installateur de l'App Installer à partir du dépôt GitHub de Microsoft
+    Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle" -OutFile "Microsoft.DesktopAppInstaller.appxbundle"
+
+    # Installe l'App Installer
+    Add-AppxPackage -Path .\Microsoft.DesktopAppInstaller.appxbundle
+
+    # Supprime le fichier d'installation téléchargé
+    Remove-Item -Path .\Microsoft.DesktopAppInstaller.appxbundle
+
+    Write-Output "Winget a été installé avec succès."
+}
