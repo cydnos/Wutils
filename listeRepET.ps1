6a5763e1e0015d19a32639503261feb7c9e2024e
cydnos <108056453+cydnos@users.noreply.github.com>
2024-04-08 23:33:39 +0200

    Update listeRepET.ps1

--- listeRepET.ps1
+++ listeRepET.ps1
@@ -1,16 +1,14 @@
 # Pour chaque fichier dans le répertoire courant
 Get-ChildItem -File | ForEach-Object {
-# Calcule la taille du fichier en Mo, arrondie à deux décimales
+    # Calcule la taille du fichier en Mo, arrondie à deux décimales
     $sizeInMB = [math]::Round($_.Length / 1MB, 2)
-# Obtient le nom du fichier sans son extension
-    $nameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($_.Name)
-# Crée un nouvel objet PowerShell avec les propriétés suivantes :
+    # Crée un nouvel objet PowerShell avec les propriétés suivantes :
     New-Object PSObject -Property @{
-# Nom du fichier sans extension
-        Name = $nameWithoutExtension
-# Taille du fichier en Mo
+        # Nom du fichier sans extension ($_ est une variable qui fait référence à l’objet actuel dans le pipeline (dans ce cas, chaque fichier).)
+        Name = $_.BaseName
+        # Taille du fichier en Mo
         SizeMB = $sizeInMB
-# Date de la dernière modification du fichier
+        # Date de la dernière modification du fichier
         LastWriteTime = $_.LastWriteTime
     }
 # Trie les objets par nom et redirige la sortie vers le fichier 'logiciels.txt'
