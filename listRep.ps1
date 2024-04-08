ede31cc1ac239f01ea921a1c560cdbe9793eaf29
cydnos <108056453+cydnos@users.noreply.github.com>
2024-04-08 23:35:30 +0200

    Delete listRep.ps1

--- listRep.ps1
+++
@@ -1,6 +0,0 @@
-# gets all files in the specified folder and its subfolders
-Get-ChildItem -Path 'C:\YourFolderPath' -Recurse -File | 
-# removes the file extensions from the list. $_ is a variable that refers to the current object in the pipeline, and BaseName is a property of the file that gets the name without the extension.
-ForEach-Object { $_.BaseName } | 
-# outputs the list to a file
-Out-File -FilePath '.\liste.txt'
